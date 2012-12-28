a971c5f1fec335f2559dacfa37b162e4f6e5c69b
Jeffrey Zhao <jeffz@live.com>
2012-12-28 23:06:56 +0800

    NullableKey

---
+++ src/Tmc/NullableKey.cs
@@ -0,0 +1,33 @@
+﻿namespace Tmc
+{
+    public struct NullableKey<T> {
+// ReSharper disable StaticFieldInGenericType
+        private static readonly bool CouldBeNull = typeof(T).IsClass;
+        private static readonly int NullValueHashCode = new object().GetHashCode();
+// ReSharper restore StaticFieldInGenericType
+
+        private readonly T _value;
+
+        public NullableKey(T value) {
+            _value = value;
+        }
+
+        public T Value {
+            get { return _value; }
+        }
+
+        public override bool Equals(object obj) {
+            return obj is NullableKey<T> && Equals((NullableKey<T>)obj);
+        }
+
+        public bool Equals(NullableKey<T> that) {
+            return CouldBeNull ? Equals(_value, that._value) : _value.Equals(that._value);
+        }
+
+        public override int GetHashCode() {
+// ReSharper disable CompareNonConstrainedGenericWithNull
+            return _value == null ? NullValueHashCode : _value.GetHashCode();
+// ReSharper restore CompareNonConstrainedGenericWithNull
+        }
+    }
+}
---
+++ src/Tmc/NullableKeyEqualityComparer.cs
@@ -0,0 +1,20 @@
+﻿namespace Tmc
+{
+    using System.Collections.Generic;
+
+    public class NullableKeyEqualityComparer<T> : IEqualityComparer<NullableKey<T>> {
+        private readonly IEqualityComparer<T> _valueComparer; 
+
+        public NullableKeyEqualityComparer(IEqualityComparer<T> valueComparer) {
+            _valueComparer = valueComparer;
+        }
+
+        public bool Equals(NullableKey<T> x, NullableKey<T> y) {
+            return _valueComparer == null ? x.Equals(y) : _valueComparer.Equals(x.Value, y.Value);
+        }
+
+        public int GetHashCode(NullableKey<T> obj) {
+            return _valueComparer == null ? obj.GetHashCode() : _valueComparer.GetHashCode(obj.Value);
+        }
+    }
+}
